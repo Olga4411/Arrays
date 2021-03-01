9b5a57bbd6582da21fc35d0a565e91356232fc5f
Novi_ij29@itstepru.onmicrosoft.com <Novi_ij29@itstepru.onmicrosoft.com>
2021-03-01 17:44:38 +0700

    1

--- Shift/main.cpp
+++ Shift/main.cpp
@@ -5,13 +5,34 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	const int SIZE = 10;
-	int arr[SIZE] = { 1,2,3,4,5,6,7,8,9,10 };
-	for (int i = 0; i < SIZE; i++)
+	const int n = 10;
+	int arr[n] = { 1,2,3,4,5,6,7,8,9,10 };
+	// Вывод на экран массива
+	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
 	}
 	cout << endl;
+	int number_of_shifts;// количество сдвигов
+	cout << "На сколько элементов сдвинуть массив:"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+		// СДВИГ МАССИВА
+	{
+		int buffer = arr[0];// нулевой элемент сохраняем в буфере, буфер-временное хранилище как стол
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+	}
+	// Вывод сдвинутого массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+// Вывод сдвинутого массива на экран
 	/*for (int i = 0; i < SIZE; i++)
 	{
 		cout << arr[i] << "\t";
